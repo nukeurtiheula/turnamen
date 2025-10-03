afdebc20de81a4cda1ed829abd9396cd2acf67f9
Muhammad Ikhsan <ikhsanm347@gmail.com>
2025-10-03 14:12:41 +0700

    FIX SKOR

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
+// src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner'; // <-- 1. IMPORT KOMPONEN TOASTER
+
 const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter> <App /> </BrowserRouter>
+      <BrowserRouter>
+        <App />
+        {/* ====================================================== */}
+        {/* ===== DAN TAMBAHKAN TOASTER DI SINI 👇 ===== */}
+        {/* ====================================================== */}
+        <Toaster theme="dark" position="bottom-right" /> {/* <-- 2. LETAKKAN KOMPONEN TOASTER DI SINI */}
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
